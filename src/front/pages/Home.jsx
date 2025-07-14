72d3e15dac3ec7421565fbd09ef5b681d1ef1601
jusspian <luisfjuspian@gmail.com>
2025-07-14 16:29:27 +0000

    videobackground added

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-    return <div>AMUZZ</div>;
+  return (
+    <div className="video-background">
+      <video autoPlay loop muted playsInline>
+        <source src="/fondo.mp4" type="video/mp4" />
+        Tu navegador no soporta video.
+      </video>
+      <div className="contenido-encima">
+        <h1>AMUZZ</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
