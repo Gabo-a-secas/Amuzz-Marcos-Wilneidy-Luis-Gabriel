6c7cd8aff04794847ad1897e9fd8d4e6ac7de4f9
MattJVPR <drivemarcosperez@gmail.com>
2025-08-04 15:08:41 +0000

    stripe

--- src/front/components/BackendURL.jsx
+++ src/front/components/BackendURL.jsx
@@ -1,2 +1,2 @@
-const BackendURL = import.meta.env.VITE_BACKEND_URL || "https://upgraded-couscous-pjwx7g97pjr9f97qj-3001.app.github.dev";
+const BackendURL = import.meta.env.VITE_BACKEND_URL || "https://laughing-memory-pjqr47pjw5q627g4j-3001.app.github.dev/";
 export default BackendURL;                             
